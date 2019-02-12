3a7b1bad0c0822f4d0999144d09cb2557b7c02f3
srihari198 <srihari198@gmail.com>
2019-02-12 15:04:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Welcome to new world
+            Welcome test new thing
           </p>
           <a
             className="App-link"
